287c977e8d44c7bcdab33dc6d79ba5608fd52ecb
William Sheu <sheu.william@gmail.com>
2026-03-21 18:17:00 -0700

    Update DAVE plugin author

--- src/main/Plugin.h
+++ src/main/Plugin.h
@@ -21,5 +21,5 @@ inline constexpr auto NAME = "DynamicArmorVariants"sv;
 inline constexpr REL::Version VERSION{DAVE_VERSION_MAJOR, DAVE_VERSION_MINOR,
                                       DAVE_VERSION_PATCH, 0};
 inline constexpr std::string_view VERSION_STRING{DAVE_VERSION_STRING};
-inline constexpr auto AUTHOR = "Parapets"sv;
+inline constexpr auto AUTHOR = "Parapets, PenguinToast"sv;
 } // namespace Plugin
